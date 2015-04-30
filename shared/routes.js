3e16877770f48f6bbc05cad7ec218c2d0e815805
Seb Insua <me@sebinsua.com>
2015-04-30 20:14:40 +0100

    Improved linting rules.

--- shared/routes.js
+++ shared/routes.js
@@ -1,4 +1,4 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React from 'react';
 import { Route } from 'react-router';
 
 import AppHandler from './components/app-handler';
